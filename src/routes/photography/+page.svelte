b8ec28f44965e2977d811bfced7d73a04c06ac63
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-07 21:37:47 +0100

    feat: add photo gallery with placeholder images

---
+++ src/routes/photography/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Gallery } from 'flowbite-svelte';
+
+	type Image = {
+		alt: string;
+		src: string;
+	};
+
+	let images: Image[] = [];
+
+	const imageModules = import.meta.glob('../../photo/*.jpg');
+	for (const modulePath in imageModules) {
+		imageModules[modulePath]().then(({ default: imageUrl }) => {
+			const slash = modulePath.lastIndexOf('/');
+			const dot = modulePath.lastIndexOf('.');
+			const name = modulePath.substring(slash + 1, dot);
+			console.log(`${name}: ${imageUrl}`);
+			images.push({
+				alt: name,
+				src: imageUrl
+			});
+			images = images;
+		});
+	}
+	console.log(images);
+</script>
+
+<div class='items-center p-10 '>
+	<Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-3"/>
+</div>
