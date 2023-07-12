e0289e435dd5452b56751dc66d6a00b46ff1a98e
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-12 21:58:35 +0100

    build: setup supabase db

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient('https://fzrnynqnijbwvccckmxe.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cm55bnFuaWpid3ZjY2NrbXhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkxOTQyOTIsImV4cCI6MjAwNDc3MDI5Mn0.BJCE7rof0xYe0LJo-FUDUnt3PP74aNK9NdFPJrITm_M");
