bbed1da5d25642bd1d9617defeee7b0c8b1a7471
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-17 23:47:59 +0100

    fix: fix navbar, slot, footer spacing

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,22 +3,24 @@
 	import Navbar from './navbar.svelte';
 </script>
 
-<style>
-.bg-animate {
-	background-size: 400%;
-
-	-webkit-animation: AnimationName 10s ease infinite;
-	-moz-animation: AnimationName 10s ease infinite;
-	animation: AnimationName 10s ease infinite;
-}
-</style>
-
-<div class="
-bg-gradient-to-br 
-from-white via-slate-200 to-slate-600 
-dark:from-black dark:via-slate-800 dark:to-slate-700 
-bg-animate min-h-screen h-max">
-<Navbar />
-<slot />
-<Footer />
+<div
+	class="flex flex-col
+bg-gradient-to-br
+from-white via-slate-200 to-slate-600
+dark:from-black dark:via-slate-800 dark:to-slate-700
+bg-animate min-h-screen h-max justify-between"
+>
+	<Navbar />
+	<slot />
+	<Footer />
 </div>
+
+<style>
+	.bg-animate {
+		background-size: 400%;
+
+		-webkit-animation: AnimationName 10s ease infinite;
+		-moz-animation: AnimationName 10s ease infinite;
+		animation: AnimationName 10s ease infinite;
+	}
+</style>
