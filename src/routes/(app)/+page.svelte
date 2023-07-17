66594320af8c820088b34939dc3cbff69c0c19d4
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-17 23:48:58 +0100

    feat: add some temp text to home page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,12 @@
-<div class="flex justify-center self-center">
+<div class="text-center py-40 space-y-10">
 	<p
-		class="py-80 align-middle justify-center text-black dark:text-white text-xl lg:text-8xl font-semibold p-10"
+		class="align-middle justify-center text-black dark:text-white text-4xl lg:text-8xl font-semibold"
 	>
-		Website under construction...
+		Website still under construction...
+	</p>
+	<p class="text-gray-500 dark:text-gray-300 text-xl md:text-">
+		Check out the
+		<a href="/music" class="font-semibold hover:text-cyan-400 hover:underline">Music</a>
+		page in the meantime...
 	</p>
 </div>
