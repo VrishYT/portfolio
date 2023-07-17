f893270dc2a97377d73d8dde06ef6af3b0145e57
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-17 22:43:05 +0100

    refactor: move tailwind import to root layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,9 +1,24 @@
 <script>
-	import '../../app.css';
 	import Footer from './footer.svelte';
 	import Navbar from './navbar.svelte';
 </script>
 
+<style>
+.bg-animate {
+	background-size: 400%;
+
+	-webkit-animation: AnimationName 10s ease infinite;
+	-moz-animation: AnimationName 10s ease infinite;
+	animation: AnimationName 10s ease infinite;
+}
+</style>
+
+<div class="
+bg-gradient-to-br 
+from-white via-slate-200 to-slate-600 
+dark:from-black dark:via-slate-800 dark:to-slate-700 
+bg-animate h-screen">
 <Navbar />
 <slot />
 <Footer />
+</div>
