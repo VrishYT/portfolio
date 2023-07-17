ee702876e04b8053ec2071b6cc2048e9f524d4f7
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-17 23:28:44 +0100

    fix: fix cards not fitting on screen

--- src/routes/(app)/music/+page.svelte
+++ src/routes/(app)/music/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { Button, Card } from 'flowbite-svelte';
-	import { Icon } from '@steeze-ui/svelte-icon';
-
-	import { Play } from '@steeze-ui/heroicons';
+	import { Card, Gallery } from 'flowbite-svelte';
 	import { MusicProjectType, type MusicProject } from './Utils';
 
 	export let data;
@@ -34,8 +31,9 @@
 		);
 </script>
 
-<div class="flex p-10 justify-center space-x-5">
-	{#each [...projects] as [id, { title, img, links, dateCreated, tags, visible, type }] (id)}
+<div class="flex p-10 justify-center">
+<Gallery class="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+	{#each [...projects] as [id, { title, img, dateCreated, tags, type }] (id)}
 		<Card {img} class="w-80" rounded shadow href="/music/{id}">
 			<div class="flex flex-row justify-between pb-2">
 				<h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -47,10 +45,11 @@
 			</div>
 			<div class="flex justify-between max-h-8">
 				<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-					{tags.map(tag => `#${tag}`).join(" ")}
+					{tags.map((tag) => `#${tag}`).join(' ')}
 				</p>
 				<p class="font-semibold text-lg text-right">{MusicProjectType[type]}</p>
 			</div>
 		</Card>
 	{/each}
+</Gallery>
 </div>
