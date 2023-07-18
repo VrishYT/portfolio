3abb431ffbb42672533a837e222922f55cfda49e
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-07-18 14:11:55 +0100

    feat: sort music projects by recent first

--- src/routes/(app)/music/+page.server.js
+++ src/routes/(app)/music/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-	const { data } = await supabase.from('music').select();
+	const { data } = await supabase.from('music').select().order('created_at', {ascending: false});
 	// console.log(data);
 	// console.log(error);
 	return { projects: data };
