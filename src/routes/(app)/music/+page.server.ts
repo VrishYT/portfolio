697dce95b3dc14c520431bfa13abe5f54da52989
Rishi Khiroya <rk1121@ic.ac.uk>
2023-07-22 15:07:42 +0100

    feat: adding sorting music page via url params

---
+++ src/routes/(app)/music/+page.server.ts
@@ -0,0 +1,35 @@
+import { supabase } from '$lib/supabaseClient';
+
+// enum Ordering {
+// 	date = 'created_at',
+// 	name = 'title',
+// 	type = 'type'
+// }
+
+const Ordering = {
+	date: {
+		column: 'created_at',
+		ascending: false
+	},
+	name: {
+		column: 'title',
+		ascending: true
+	},
+	type: {
+		column: 'type',
+		ascending: true
+	}
+};
+
+type MyError = (typeof Ordering)[keyof typeof Ordering];
+
+export async function load({ url }) {
+	const order =
+		Ordering[url.searchParams.has('orderBy') ? url.searchParams.get('orderBy') : 'date'];
+	const ascending: boolean = url.searchParams.has('asc')
+		? url.searchParams.get('asc') === 'true'
+		: order.ascending;
+	let { data } = await supabase.from('music').select().order(order.column, { ascending });
+
+	return { projects: data };
+}
