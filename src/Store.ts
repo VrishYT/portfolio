aae622f764e9495ed403ebfcfb8dcd9beca4d22e
Rishi Khiroya <rk1121@ic.ac.uk>
2023-12-30 18:29:41 +0000

    chore: Remove unused Store.ts

--- src/Store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-import type { MusicProject } from "./routes/(app)/music/Utils";
-
-export const musicProjectsStore = writable(new Map<string, MusicProject>()); 
-
