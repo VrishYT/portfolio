a15c077ea514c88411ac77c2cf432577e2aea174
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-02 23:36:47 +0000

    feat: Add link to coding page on home page.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -6,7 +6,9 @@
 	</p>
 	<p class="text-gray-500 dark:text-gray-300 text-xl md:text-">
 		Check out the
-		<a href="/music" class="font-semibold hover:text-cyan-400 hover:underline">Music</a>
+		<a href="/music" class="font-semibold text-purple-700 dark:text-purple-400 hover:text-cyan-400 hover:underline">Music</a>
+		or  
+		<a href="/coding" class="font-semibold text-purple-700 dark:text-purple-400 hover:text-cyan-400 hover:underline">Coding</a>
 		page in the meantime...
 	</p>
 </div>
